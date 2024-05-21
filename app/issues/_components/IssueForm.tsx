911735ae5b4230fe3850b28119cf1327b6d9acbc
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-05-21 12:42:21 +0800

    Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,20 +1,16 @@
 "use client"
+import { ErrorMessage, Spinner } from "@/app/components"
+import { issueSchema } from "@/app/validationSchemas"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Issue } from "@prisma/client"
 import { Button, Callout, TextField } from "@radix-ui/themes"
-import "easymde/dist/easymde.min.css"
-import { Controller, useForm } from "react-hook-form"
 import axios from "axios"
+import "easymde/dist/easymde.min.css"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { issueSchema } from "@/app/validationSchemas"
+import { Controller, useForm } from "react-hook-form"
+import SimpleMDE from "react-simplemde-editor"
 import { z } from "zod"
-import { Spinner, ErrorMessage } from "@/app/components"
-import dynamic from "next/dynamic"
-import { Issue } from "@prisma/client"
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-})
 
 type IssueFormData = z.infer<typeof issueSchema>
 
