b26d032e12b67aff30f659f0d0cea68994bc3b78
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-05-22 16:10:44 +0800

    Remove duplicate skeletons

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Spinner } from "@/app/components"
-import { TrashIcon } from "@radix-ui/react-icons"
 import { AlertDialog, Button, Flex } from "@radix-ui/themes"
 import axios from "axios"
 import { useRouter } from "next/navigation"
@@ -15,7 +14,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     try {
       setDeleting(true)
       await axios.delete("/api/issues/" + issueId)
-      router.push("/issues")
+      router.push("/issues/list")
       router.refresh()
     } catch (error) {
       setDeleting(false)
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@/app/components"
 import prisma from "@/prisma/client"
 import { Table } from "@radix-ui/themes"
-import IssueStatusBadge from "../components/IssueStatusBadge"
+import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import IssueActions from "./IssueActions"
 
 const IssuesPage = async () => {
