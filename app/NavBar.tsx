199894daaa4d1c25dfc50f5e3e9a6bc015a7a95b
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-05-23 20:56:15 +0800

    Add a loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,6 +13,7 @@ import {
   Text,
 } from "@radix-ui/themes"
 import { useSession } from "next-auth/react"
+import { Skeleton } from "@/app/components"
 
 const NavBar = () => {
   return (
@@ -61,6 +62,8 @@ const NavLinks = () => {
 
 const AuthStatus = () => {
   const { status, data: session } = useSession()
+
+  if (status === "loading") return <Skeleton width={"3rem"} />
 
   if (status === "unauthenticated")
     return (
