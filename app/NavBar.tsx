f99b4b5627cf4a49cf96a9fc9e769b91bf560a7d
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-05-23 20:17:23 +0800

    Add a dropdown menu to show the current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,14 @@ import { usePathname } from "next/navigation"
 import React from "react"
 import { AiFillBug } from "react-icons/ai"
 import classnames from "classnames"
-import { Box, Container, Flex } from "@radix-ui/themes"
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes"
 import { useSession } from "next-auth/react"
 
 const NavBar = () => {
@@ -15,6 +22,7 @@ const NavBar = () => {
     { label: "Dasboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
   ]
+  const userSession = session?.user
 
   return (
     <nav className="border-b mb-5 px-5 py-3">
@@ -43,11 +51,29 @@ const NavBar = () => {
           </Flex>
           <Box>
             <Box>
+              {status === "authenticated" && (
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Avatar
+                      src={userSession!.image!}
+                      fallback="?"
+                      size={"2"}
+                      radius="full"
+                      className="cursor-pointer"
+                    />
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text>{userSession?.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Link href={"/api/auth/signout"}>Log out</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
+              )}
               {status === "unauthenticated" && (
                 <Link href={"/api/auth/signin"}>Log in</Link>
-              )}
-              {status === "authenticated" && (
-                <Link href={"/api/auth/signout"}>Log out</Link>
               )}
             </Box>
           </Box>
