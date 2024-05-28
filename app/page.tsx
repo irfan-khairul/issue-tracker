08e7ddecb296f4c5a6d3aba8232c951b6273e648
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-05-29 02:22:53 +0800

    Build the IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client"
 import IssueSummary from "./IssueSummary"
 import LatestIssues from "./LatestIssues"
 import { Flex } from "@radix-ui/themes"
+import IssueChart from "./IssueChart"
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } })
@@ -12,8 +13,9 @@ export default async function Home() {
 
   return (
     <Flex direction={"column"} gap={"3"}>
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-      <LatestIssues />
+      {/* <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues /> */}
+      <IssueChart open={open} inProgress={inProgress} closed={closed} />
     </Flex>
   )
 }
